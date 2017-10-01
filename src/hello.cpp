9be2ebbef8f13257a079dd9f365af41049440dce
Zhou (Jason) Shi <zhoushihn@gmail.com>
2017-10-01 11:48:00 -0700

    add say_datetime()

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,6 +1,8 @@
+#include <chrono>
 #include <iostream>
 
 int say_hello(std::string);
+int say_datetime(void);
 
 int main(int argc, char** argv){
 	std::string whomever = "";
@@ -10,8 +12,9 @@ int main(int argc, char** argv){
 	} else {
 		whomever = std::string(argv[1]);
 	}
-
+	
 	say_hello(whomever);
+	say_datetime();
 	return 0;
 }
 
@@ -19,3 +22,15 @@ int say_hello(std::string whom){
 	std::cout<<"hello "<<whom<<"!"<<std::endl;
 	return 0;
 }
+
+int say_datetime(){
+	std::chrono::time_point<std::chrono::system_clock> s_now;
+	
+	s_now = std::chrono::system_clock::now();
+	std::time_t t_now = std::chrono::system_clock::to_time_t(s_now);
+
+	std::cout<<"now is "<<std::ctime(&t_now)<<std::endl;
+
+	return 0;	
+}
+
